61f3fae7b8bab3cff13bc34dd74cd3389b7a88e0
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-10-11 09:41:11 -0600

    updated paths in wikipedia serve.sh
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- docs/wikipedia/serve.sh
+++ docs/wikipedia/serve.sh
@@ -2,4 +2,4 @@
 
 root_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker run -d --name=wikipedia -v ${root_dir}/docs:/data -p 8080:80 localhost:5000/kiwix/kiwix-serve wikipedia_en_all_nopic.zim
+docker run -d --name=wikipedia -v ${root_dir}/docs:/data -p 8080:80 localhost:5000/kiwix/kiwix-serve wikipedia.zim 
