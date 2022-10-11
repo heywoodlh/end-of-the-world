3921bf11b07ebedd7c801732c6008bfbb3705b67
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-10-11 11:59:46 -0600

    updated docs
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ root_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ## Setup Docker registry
 "${root_dir}"/docker/registry.sh
 
-## Download and serve wikipedia
-"${root_dir}"/docs/wikipedia/sync.sh
-"${root_dir}"/docs/wikipedia/serve.sh
+## Download and serve Kiwix content
+"${root_dir}"/docs/kiwix/sync.sh
+"${root_dir}"/docs/kiwix/serve.sh
 
 ## Download and serve pacman repository
 "${root_dir}"/repos/pacman/sync.sh 
