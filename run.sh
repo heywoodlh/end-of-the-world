ee0a2f9d3263b2156ec9350ba14841567bce68ae
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-10-10 23:28:43 -0600

    updated paths in run.sh
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- run.sh
+++ run.sh
@@ -10,17 +10,17 @@ root_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 "${root_dir}"/docs/wikipedia/serve.sh
 
 ## Download and serve pacman repository
-"${root_dir}"/docs/pacman/sync.sh 
-"${root_dir}"/docs/pacman/serve.sh 
+"${root_dir}"/repos/pacman/sync.sh 
+"${root_dir}"/repos/pacman/serve.sh 
 
 ## Download and serve ubuntu repository
-"${root_dir}"/docs/ubuntu/sync.sh 
-"${root_dir}"/docs/ubuntu/serve.sh 
+"${root_dir}"/repos/ubuntu/sync.sh 
+"${root_dir}"/repos/ubuntu/serve.sh 
 
 ## Download and serve f-droid repository
-"${root_dir}"/docs/f-droid/sync.sh 
-"${root_dir}"/docs/f-droid/serve.sh 
+"${root_dir}"/repos/f-droid/sync.sh 
+"${root_dir}"/repos/f-droid/serve.sh 
 
 ## Download and serve termux repository
-"${root_dir}"/docs/termux/sync.sh 
-"${root_dir}"/docs/termux/serve.sh 
+"${root_dir}"/repos/termux/sync.sh 
+"${root_dir}"/repos/termux/serve.sh 
