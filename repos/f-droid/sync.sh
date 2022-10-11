4b7374e22bc7832bbf7089ce9d0581c1c8fb954c
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-10-11 09:45:24 -0600

    updated sync.sh
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- repos/f-droid/sync.sh
+++ repos/f-droid/sync.sh
@@ -2,4 +2,4 @@
 
 root_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-rsync -aHS  --delete --delete-delay --info=progress2 plug-mirror.rcac.purdue.edu::fdroid/repo "${root_dir}"/repo/
+rsync -aHS  --delete --delete-delay --info=progress2 plug-mirror.rcac.purdue.edu::fdroid/repo "${root_dir}"
